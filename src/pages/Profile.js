8f3c42dcd2e5c324c837c6cb88490003aae4e683
Daniel Yabu <danielyabu@yahoo.com>
2022-05-25 14:21:41 -0300

    Requisito 13 faltando um subrequisito para terminar

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    loading: false,
+    user: '',
+    email: 'email@test.com',
+    descricao: 'Lorem ipsum',
+    imagem: 'url-to-image',
+  }
+
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    const userInfo = await getUser();
+    this.setState({
+      loading: false,
+      user: userInfo.name,
+      email: userInfo.email,
+      descricao: userInfo.description,
+      imagem: userInfo.image,
+    });
+  }
+
   render() {
+    const { imagem, user, loading, email, descricao } = this.state;
+    if (loading) {
+      return <span>Carregando...</span>;
+    }
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        <img src={ imagem } alt="imagem de perfil" data-testid="profile-image" />
+        <Link to="/profile/edit">Editar perfil</Link>
+        <p className="user-name">Nome</p>
+        <p>{ user }</p>
+        <p className="email">Email</p>
+        <p>{ email }</p>
+        <p className="descricao">Descrição</p>
+        <p>{ descricao }</p>
       </div>
     );
   }
--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,12 +1,72 @@
+/* eslint-disable max-len */
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
+  state = {
+    loading: true,
+    user: '',
+    email: '',
+    descricao: '',
+    imagem: '',
+  }
+
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    const userInfo = await getUser();
+    this.setState({
+      user: userInfo.name,
+      loading: false,
+      email: userInfo.email,
+      descricao: userInfo.descricao,
+      imagem: userInfo.imagem,
+    });
+  }
+
   render() {
+    const { user, loading, descricao, email } = this.state;
+    if (loading) {
+      return <span>Carregando...</span>;
+    }
     return (
       <div data-testid="page-profile-edit">
         <Header />
-        Profile edit
+        <form>
+          <label htmlFor="username">
+            Nome:
+            <input
+              type="text"
+              data-testid="edit-input-name"
+              placeholder={ user }
+              id="username"
+            />
+          </label>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              data-testid="edit-input-email"
+              placeholder={ email }
+              id="email"
+            />
+          </label>
+          <label htmlFor="descricao">
+            Descrição:
+            <input
+              type="textarea"
+              data-testid="edit-input-description"
+              placeholder={ descricao }
+              id="descricao"
+            />
+          </label>
+          <label htmlFor="imagem">
+            Imagem de perfil:
+            <input type="text" data-testid="edit-input-image" id="imagem" />
+          </label>
+        </form>
       </div>
     );
   }
